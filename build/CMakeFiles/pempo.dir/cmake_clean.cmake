976774096e9044681a7c0bc0637173479a775c9f
Lucas Girouard-Stranks <lustranks@gmail.com>
2015-08-11 11:01:26 -0700

    working

---
+++ build/CMakeFiles/pempo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pempo.dir/main.cpp.o"
+  "../bin/libpempo.pdb"
+  "../bin/libpempo.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pempo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
