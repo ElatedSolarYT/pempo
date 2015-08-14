9d6c6012bc7688a8041c197e78f73278968f2e71
Lucas Girouard-Stranks <lustranks@gmail.com>
2015-08-13 21:37:32 -0700

    Add example Mono library for injection

---
+++ MonoLibrary/Pempo/EntryPointAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Pempo
+{
+	[AttributeUsage(AttributeTargets.Assembly)]
+	public class EntryPointAttribute : Attribute
+	{
+		public string EntryPointMethod { get; private set;}
+
+		public EntryPointAttribute(string entryPointMethod)
+		{
+			EntryPointMethod = entryPointMethod;
+		}
+	}
+}
+
---
+++ MonoLibrary/Pempo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Pempo
+{
+	public static class Program
+	{
+		public static void EntryPoint()
+		{
+			Console.WriteLine("Hello Mono world!");
+		}
+	}
+}
+
