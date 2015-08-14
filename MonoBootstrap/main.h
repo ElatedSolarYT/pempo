d8e659595f6ffa3f3cf31b80954d195530eaec29
Lucas Girouard-Stranks <lustranks@gmail.com>
2015-08-13 21:40:59 -0700

    Add MonoBootsrap dynamic library

--- MonoBootstrap/main.h
+++ MonoBootstrap/main.h
@@ -15,12 +15,6 @@
 #include <mach/mach.h>
 #include <mach/thread_act.h>
 #include <mach/mach_init.h>
-#include "config.h"
-
-// Preprocessor macro to convert string preprocessor definitions into C strings
-
-#define STRINGIFY(x) #x
-#define TOSTRING(x) STRINGIFY(x)
 
 // External symbols
-extern "C" void EntryPoint(const std::string mono_assembly_path);// __attribute__((visibility("default")));
+extern "C" void EntryPoint(const std::string mono_assembly_path);
