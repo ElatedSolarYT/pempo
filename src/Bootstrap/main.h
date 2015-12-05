671ec77572beb50ae993326a53f4f22a63a1320f
Lucas Girouard-Stranks <lustranks@gmail.com>
2015-12-04 21:19:50 -0800

    Reformat header

--- src/Bootstrap/main.h
+++ src/Bootstrap/main.h
@@ -7,5 +7,5 @@
 #include <CoreFoundation/CoreFoundation.h>
 
 extern "C" void __pthread_set_self(void *);
-extern "C" void Bootstrap(ptrdiff_t code_offset, void * param_block, size_t param_size, void * dummy_pthread_data);
-extern "C" void UserNotificationDisplayNotice(const char * header, const char * message, int type);
+extern "C" void Bootstrap(ptrdiff_t code_offset, void *param_block, size_t param_size, void *dummy_pthread_data);
+extern "C" void UserNotificationDisplayNotice(const char *header, const char *message, int type);
