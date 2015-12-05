9fe4b59e33e969150b30cc1c4926a94e7d39b480
Lucas Girouard-Stranks <lustranks@gmail.com>
2015-12-04 21:03:21 -0800

    Cleanup includes

--- src/MonoBootstrap/main.h
+++ src/MonoBootstrap/main.h
@@ -1,16 +1,9 @@
 #pragma once
 
 #include <stdlib.h>
-#include <dlfcn.h>
-#include <mach/error.h>
 #include <mono/jit/jit.h>
 #include <mono/metadata/assembly.h>
 #include <mono/metadata/debug-helpers.h>
 #include <mono/metadata/reflection.h>
-#include <mach/mach_init.h>
-#include <pthread.h>
-#include <mach/mach.h>
-#include <mach/thread_act.h>
-#include <mach/mach_init.h>
 
 extern "C" void EntryPoint(const char* mono_assembly_path);
