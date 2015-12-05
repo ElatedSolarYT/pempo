5530bd3e45f26caf2317b3f319bac9f809776898
Lucas Girouard-Stranks <lustranks@gmail.com>
2015-12-04 21:00:35 -0800

    Move source code to /src/

--- src/MonoBootstrap/main.h
+++ src/MonoBootstrap/main.h
@@ -1,12 +1,8 @@
 #pragma once
 
-// Included header files
-
-#include <string>
 #include <stdlib.h>
 #include <dlfcn.h>
 #include <mach/error.h>
-#include <mach_override.h>
 #include <mono/jit/jit.h>
 #include <mono/metadata/assembly.h>
 #include <mono/metadata/debug-helpers.h>
@@ -17,8 +13,4 @@
 #include <mach/thread_act.h>
 #include <mach/mach_init.h>
 
-// External symbols
-extern "C" void EntryPoint(const std::string mono_assembly_path);
-
-#define STRINGIFY(x) #x
-#define TOSTRING(x) STRINGIFY(x)
+extern "C" void EntryPoint(const char* mono_assembly_path);
