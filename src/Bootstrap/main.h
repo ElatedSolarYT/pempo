0b2f0c1edd6ea7995207221f8f93ef9a045c5a83
Lucas Girouard-Stranks <lustranks@gmail.com>
2015-12-04 22:31:27 -0800

    Update extern

--- src/Bootstrap/main.h
+++ src/Bootstrap/main.h
@@ -8,4 +8,4 @@
 
 extern "C" void __pthread_set_self(void *);
 extern "C" void Bootstrap(ptrdiff_t code_offset, void *param_block, size_t param_size, void *dummy_pthread_data);
-extern "C" void UserNotificationDisplayNotice(const char *header, const char *message, int type);
+extern "C" void UserNotificationDisplayNotice(const char *header, const char *message, CFOptionFlags type);
