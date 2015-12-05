9048a9341eed7b10c1e016febaf9d07578b06d3a
Lucas Girouard-Stranks <lustranks@gmail.com>
2015-12-04 22:17:20 -0800

    Cleanup MonoTestLibrary

---
+++ src/MonoTestLibrary/MonoTestLibrary/Program.cs
@@ -0,0 +1,29 @@
+﻿#region
+
+using System;
+using System.Runtime.InteropServices;
+
+#endregion
+
+namespace MonoTestLibrary
+{
+    public static class Program
+    {
+        public enum UserNotificationType
+        {
+            Stop = 0,
+            Alert = 1,
+            Caution = 2,
+            Plain = 3
+        }
+
+        [DllImport("libbootstrap.dylib", CharSet = CharSet.Auto)]
+        private static extern void UserNotificationDisplayNotice(string title, string message, UserNotificationType type);
+
+        public static void EntryPoint()
+        {
+            Console.WriteLine("Hello Mono world!");
+            UserNotificationDisplayNotice("Hello", "Hello Mono world!", UserNotificationType.Alert);
+        }
+    }
+}
