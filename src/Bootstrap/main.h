c594e8daf1755507068250636431c951997bf7f4
Lucas Girouard-Stranks <lustranks@gmail.com>
2015-12-10 12:59:48 -0800

    Remove version, include mach_override

--- src/Bootstrap/main.h
+++ src/Bootstrap/main.h
@@ -5,7 +5,9 @@
 #include <dlfcn.h>
 #include <mach/mach.h>
 #include <CoreFoundation/CoreFoundation.h>
+#include "mach_override.h"
 
 extern "C" void __pthread_set_self(void *);
 extern "C" void Bootstrap(ptrdiff_t code_offset, void *param_block, size_t param_size, void *dummy_pthread_data);
 extern "C" void UserNotificationDisplayNotice(const char *header, const char *message, CFOptionFlags type);
+extern "C" mach_error_t mach_override_ptr(void *originalFunctionAddress, const void *overrideFunctionAddress, void **originalFunctionReentryIsland);
